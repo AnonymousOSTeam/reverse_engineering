a5561cfe3b3cf27c888013252cefd1f2c6a44362
DiegoDev <diegorego516@gmail.com>
2026-07-18 19:43:26 -0300

    Add files via upload

---
+++ reverse_engineering_tool.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define REVERSE_ENGINEERING_TYPE heuristics
+#define MALWARE_ANALYSIS_TOOL malware
+#define REVERSE_ENGINEERING_TOOL dataflows
+
+int main(int argc, char *argv[]){
+char *ferramenta_de_engenharia_reversa1 = "tipo_de_malware";
+char *ferramenta_de_engenharia_reversa2 = "tipo_de_spyware";
+char *ferramenta_de_engenharia_reversa3 = "tipo_de_trojan";
+
+int options;
+if ("options (Y/N?): ");
+int verificando_por_tipos_de_malware;
+scanf("%s", verificando_por_tipos_de_malware);
+printf("verificando por tipos de malware.\n");
+printf("ferramenta de engenharia reversa feita em C.\n");
+return 0;
+}
